277f002885bb478a66e1929317e88c00e99e9f8e
Anna Zielinska <115597522+annziel@users.noreply.github.com>
2023-01-12 19:11:32 +0100

    Add styling and setup for watchlist updating

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const searchInput = document.getElementById("search-input")
-const searchBtn = document.getElementById("search-btn")
 const main = document.querySelector("main")
 const watchlist = []
 
-searchBtn.addEventListener("click", searchMovies)
+
+document.addEventListener("click", e => {
+    e.target.id === "search-btn" ? searchMovies() :
+    e.target.classList.contains("watchlist-area") ? updateWatchlist(e.target.id) : null
+})
+
+
 searchInput.addEventListener("keydown", (e) => {
     if (e.code === "Enter" && !e.shiftKey) {
         e.preventDefault()
@@ -35,36 +40,42 @@ function createMovieHtml(movie) {
     }
 
     const oneMovieHtml = `
-        <div class="movie-poster" style="background-image:url(${movie.Poster}")></div>
-        <div class="movie-details">
-            <div class="movie-titleline>
-                <p class="movie-title">${movie.Title}</p>
-                <i class="fa-solid fa-star"></i>
-                <p class="movie-rating">${movie.imdbRating}</p>
+        <div class="movie-container">
+            <div class="movie-poster" style="background-image:url(${movie.Poster}")></div>
+            <div class="movie-details">
+                <div class="movie-titleline">
+                    <p class="movie-title">${movie.Title}</p>
+                    <i class="fa-solid fa-star"></i>
+                    <p class="movie-rating">${movie.imdbRating}</p>
+                </div>
+                <div class="movie-watchlistline">
+                    <p class="movie-runtime">${movie.Runtime}</p>
+                    <p class="movie-genre">${movie.Genre}</p>
+                    ${watchlistHtml(isOnWatchlist, movie.Title)}
+                </div>
+                <p class="movie-plot">${movie.Plot}</p>
             </div>
-            <div class="movie-watchlistline">
-                <p class="movie-runtime">${movie.Runtime}</p>
-                <p class="movie-genre">${movie.Genre}</p>
-                ${watchlistHtml(isOnWatchlist)}
-            </div>
-            <p class="movie-plot">${movie.Plot}</p>
         </div>
         <hr>
     `
     moviesHtml += oneMovieHtml
 }
 
-function watchlistHtml(boolean) {
+function watchlistHtml(boolean, title) {
     if (boolean) {
         return `
-            <i class="fa-solid fa-circle-minus" class="watchlist-icon"></i>
-            <p class="watchlist-text">Remove</p>
+            <div class="watchlist-area" id=${title}>
+                <i class="fa-solid fa-circle-minus" class="watchlist-icon"></i>
+                <p class="watchlist-text">Remove</p>
+            </div>
         `
     }
     else {
         return `
-            <i class="fa-solid fa-circle-plus" class="watchlist-icon"></i>
-            <p class="watchlist-text">Watchlist</p>
+            <div class="watchlist-area" id=${title}>
+                <i class="fa-solid fa-circle-plus" class="watchlist-icon"></i>
+                <p class="watchlist-text">Watchlist</p>
+            </div>
         `
     }
 }
@@ -79,4 +90,13 @@ function renderMoviesList() {
 function showErrorMessage() {
     main.classList.add("clear")
     main.innerHTML = `<p>Unable to find what you’re looking for.<br>Please try another search.</p>`
+}
+
+function updateWatchlist(movieId) {
+    console.log("updateWatchlist started")
+    const arrayToSearch = [
+        { Title: movieId}
+    ]
+    getMoviesDetails(arrayToSearch)
+    console.log("updateWatchlist ended")
 }
